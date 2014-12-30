2ad1e193837a2ca831e6975fa2702225f69c1c70
JonGonard <yonatan.horovitz@gmail.com>
2014-12-30 22:45:07 +0200

    add styles

---
+++ src/Sela.LibraryExample.UI/Helpers/IsLentToFontWeightConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Sela.LibraryExample.UI.Helpers
+{
+  public class IsLentToFontWeightConverter : IValueConverter
+  {
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+      var isLent = (bool) value;
+
+      return isLent ? FontWeights.Bold : FontWeights.Normal;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
